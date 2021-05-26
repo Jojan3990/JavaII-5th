3a01faf2de02015f4c5e4f1eef06ae79daceace6
Jojan3990 <raijozan2443@gmail.com>
2021-05-26 18:52:11 +0545

    assignment1

--- Applet/FirstApplet/FirstApplet.java
+++
@@ -1,9 +0,0 @@
-
-import java.awt.*;
-import java.applet.*;
-
-public class FirstApplet extends Applet{
-    public void paint(Graphics g){
-        g.drawString("A test applet", 100, 100);
-    }
-}
